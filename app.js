49f0103bf1989d90d96f20aa4e9dcd070109e9ec
Ivan Caceres <dg2.ivancaceres@gmail.com>
2021-12-19 07:44:20 -0300

    carrito

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const carritoTabla = document.querySelector('.carritoTabla');
 const contenidocarrito = document.querySelector('#lista-carrito Tbody')
 const cursosEnLinea = document.querySelector('.cursosEnLinea');
 
+
 let ArticulosCarrito = []; //array donde iran los articulos del carrito
 
 cargarEventListeners();
@@ -29,12 +30,30 @@ function leerDatosCurso(curso){
     const infoCurso ={
         imagen: curso.querySelector('.cardImagen img').src , /*src para extraer la imagen */
         nombre: curso.querySelector('.CardNombreCurso p').textContent, /*textContent para extraer el texto */
-        precio: curso.querySelector('.card p:nth-child(3)').textContent
+        precio: curso.querySelector('.card p:nth-child(3)').textContent,
+        id: curso.querySelector('button').getAttribute('data-id'),
+        cantidad:1
+
     }
     console.log(infoCurso)
-    //agregar elementos al array del carrito
+    //reviso si existen dos cursos iguales
+    const existe = ArticulosCarrito.some(cursoExiste => cursoExiste.id === infoCurso.id);
+    if(existe){
+        //actualizo la cantidad
+        const cursos = ArticulosCarrito.map( cursoExiste =>{
 
-    ArticulosCarrito = [...ArticulosCarrito, infoCurso]; //tomo una copia del array y le agrego el objeto infoCurso
+            if( cursoExiste.id === infoCurso.id){
+                cursoExiste.cantidad++;
+                return cursoExiste
+            }else{
+                return cursoExiste;
+            }
+        });
+        ArticulosCarrito = [...cursos];
+    }else{
+        //agregar elementos al array del carrito
+        ArticulosCarrito = [...ArticulosCarrito, infoCurso]; //tomo una copia del array y le agrego el objeto infoCurso
+    }
 
     carritoHTML();
 
@@ -49,17 +68,14 @@ function carritoHTML(){
     ArticulosCarrito.forEach( ( curso )=>{
         const row = document.createElement('tr');
         row.innerHTML = `
-        <td>
-        ${curso.nombre}               
-         </td>
-         <td>
-         <img src="${curso.imagen} " style="width: 100%;" alt="">
-                      
-         </td>
-         <td>
-        ${curso.precio}               
-         </td>
-        
+        <td> <img src="${curso.imagen} " style="width: 90%;" alt=""> </td>
+
+        <td>${curso.nombre} </td>
+
+         <td> ${curso.precio}</td>
+         <td> ${curso.cantidad}</td>
+         <td> <button id-curso='${curso.id}' > x </button></td>
+
         `;
         //agrega html en el carrito
         contenidocarrito.appendChild(row);
