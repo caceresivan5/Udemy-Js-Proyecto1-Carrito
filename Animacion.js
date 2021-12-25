599ccc11e5a84872cc3247e8a17dcfc6251aecb0
Ivan Caceres <dg2.ivancaceres@gmail.com>
2021-12-25 12:32:40 -0300

    aplicando css

---
+++ Animacion.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.bannerPrincipal2', {
+    duration: '3000',
+    origin: 'right',
+    distance: '10rem'
+});
+
+sr.reveal('.parrafo', {
+    duration: '3000'
+});
+
+sr.reveal('.cursosInfo', {
+    duration: '3000',
+    origin: 'left',
+    distance: '10rem'
+});
+
+sr.reveal('.cardInfo', {
+    duration: '3000',
+    origin: 'left',
+    distance: '10rem'
+});
+
+sr.reveal('.frase img', {
+    duration: '3000'
+});
