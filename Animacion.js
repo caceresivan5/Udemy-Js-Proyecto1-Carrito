6c889312bf384fef7dfe34996ba31f2792c99b2b
Ivan Caceres <dg2.ivancaceres@gmail.com>
2021-12-25 21:06:34 -0300

    aplicando AOS

--- Animacion.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-    let animacion = document.querySelector('.animado');
-    let positionObj = animacion.getBoundingClientRect().top;
-    let tamanioPantalla = window.innerHeight / 2;
-    if(positionObj < tamanioPantalla){
-        animacion.style.animation = 'mover 1s ease-out'
-    }
-})
